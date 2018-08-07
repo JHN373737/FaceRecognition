192e23f1b09ed53d3e8b386bd7267c0ea61e9e09
Jason Nguyen <jasonhnguyen00@gmail.com>
2018-08-07 00:25:19 -0500

    empty

---
+++ FaceRecognizer.py
@@ -0,0 +1,15 @@
+import FaceRecognition.FaceDetector
+import cv2
+import os
+import numpy as np
+
+
+
+
+
+
+'''
+image_path = "opencv_stuff/samples/data/lena.jpg"
+opencv_classifier_path = "opencv_stuff/lbpcascades/lbpcascade_frontalface_improved.xml"
+FaceRecognition.FaceDetector.run_detection(image_path, opencv_classifier_path)
+'''
