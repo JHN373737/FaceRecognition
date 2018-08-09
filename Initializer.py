1af9bba59b91786e1d6d779d183fbde7bc7dacdf
Jason Nguyen <jasonhnguyen00@gmail.com>
2018-08-09 00:47:08 -0500

    added try for grayscale conversion to catch already grayscale images

--- Initializer.py
+++ Initializer.py
@@ -7,6 +7,14 @@ def display_img(title, img):
     cv2.waitKey(0)  # wait for x ms -> 0 means wait until a key is pressed
     cv2.destroyAllWindows()
 
+def cvt2GRAY(img):
+    try:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        return gray_img
+    except:             # if already gray just return it
+        return img
+
+
 def load_image(image_path):
     if os.path.exists(image_path):
         img = cv2.imread(str(image_path))  # load image
@@ -23,3 +31,4 @@ def load_detection_classifier(opencv_classifier_path):
         classifier = None
         print(str(opencv_classifier_path) + " not found bruhh")
     return classifier
+
